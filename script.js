035aeb68ebcb1e2567d5eaf7dcb10e63776dec54
Vitor Gabriel <94617975+vitorgsn@users.noreply.github.com>
2022-12-29 08:28:53 -0300

    Update script.js

--- script.js
+++ script.js
@@ -55,3 +55,4 @@ document.querySelector('.again').addEventListener('click', function() {
     document.querySelector('body').style.backgroundColor = '#222'
     document.querySelector('.number').style.width = '15rem'
 })
+
